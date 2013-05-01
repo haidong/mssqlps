6f8df7e491fa9598a54eb74bc46b931cda1c08d1
Haidong Ji <Haidong.Ji@gmail.com>
2013-05-01 16:31:32 -0500

    fixed issues when server doesn't have sql server instance installed

--- DBAMetrics/insertInstance.ps1
+++ DBAMetrics/insertInstance.ps1
@@ -3,13 +3,15 @@ function getSqlInstanceName($ComputerName)
 	$a = Get-Service -ComputerName $ComputerName | where {($_.Name -like
     'mssql$*') -or ($_.Name -eq 'mssqlserver')}
     $instanceNameArray = New-Object System.Collections.ArrayList
-	$a | foreach {
-        if ($_.Name -eq 'mssqlserver') {
-            [void] $instanceNameArray.add($ComputerName)
-        }
-        else {
-            [void] $instanceNameArray.add($ComputerName + "\" +
-            $_.Name.split("$")[1])
+    if ($a -ne $null) {
+        $a | foreach {
+            if ($_.Name -eq 'mssqlserver') {
+                [void] $instanceNameArray.add($ComputerName)
+            }
+            else {
+                [void] $instanceNameArray.add($ComputerName + "\" +
+                $_.Name.split("$")[1])
+            }
         }
     }
     $instanceNameArray
@@ -21,8 +23,10 @@ $a | foreach {
     $b = getSqlInstanceName($_.HostName)
     $b | foreach {
         $InstanceName = $_
-        $sql = "EXEC Windows.Instance_Insert $HostID, '$InstanceName'"
-        Invoke-Sqlcmd -Query $sql -ServerInstance "sql1" -Database `
-        "DBAMetrics"
+        if ($InstanceName -ne $null) {
+            $sql = "EXEC Windows.Instance_Insert $HostID, '$InstanceName'"
+            Invoke-Sqlcmd -Query $sql -ServerInstance "sql1" -Database `
+            "DBAMetrics"
+        }
     }
 }
