77e7c8be76ab94f0027cba052e2d88b69526427d
Haidong Ji <Haidong.Ji@gmail.com>
2013-10-08 20:29:41 -0500

    Changed 2 variable names so they are more meaningful

--- SysMetrics/updateHost.ps1
+++ SysMetrics/updateHost.ps1
@@ -1,41 +1,41 @@
-$a = Invoke-Sqlcmd -Query "exec Windows.Host_Select_HostID_HostName" -ServerInstance "sql1" -Database "SysMetrics"
-$a | ForEach-Object {
+$HostList = Invoke-Sqlcmd -Query "exec Windows.Host_Select_HostID_HostName" -ServerInstance "sql1" -Database "SysMetrics"
+$HostList | ForEach-Object {
     $HostName = $_.HostName
     $HostID = $_.HostID
 
     try {
-       $b = get-wmiobject -computername $HostName -Class Win32_ComputerSystem
+       $WmiResults = get-wmiobject -computername $HostName -Class Win32_ComputerSystem
         }
     catch [Exception] {
        continue
     }
 
-    $Domain = $b.Domain
-    $HardwareVendor = $b.Manufacturer
-    $HardwareModel = $b.Model
-    $MemorySizeGB = ($b.TotalPhysicalMemory / 1gb) + 1
+    $Domain = $WmiResults.Domain
+    $HardwareVendor = $WmiResults.Manufacturer
+    $HardwareModel = $WmiResults.Model
+    $MemorySizeGB = ($WmiResults.TotalPhysicalMemory / 1gb) + 1
 
     try {
-       $b = get-wmiobject -computername $HostName -Class Win32_Processor | select -first 1
+       $WmiResults = get-wmiobject -computername $HostName -Class Win32_Processor | select -first 1
         }
     catch [Exception] {
        continue
     }
 
-    $CPUType = $b.name
-    $CoreCount = $b.NumberOfCores
+    $CPUType = $WmiResults.name
+    $CoreCount = $WmiResults.NumberOfCores
 
     try {
-       $b = get-wmiobject -computername $HostName -Class Win32_OperatingSystem
+       $WmiResults = get-wmiobject -computername $HostName -Class Win32_OperatingSystem
         }
     catch [Exception] {
        continue
     }
 
-    $OS = $b.Caption
-    $OSArchitecture = $b.OSArchitecture
-    $OSServicePack = $b.CSDVersion
-    $OSVersionNumber = $b.Version
+    $OS = $WmiResults.Caption
+    $OSArchitecture = $WmiResults.OSArchitecture
+    $OSServicePack = $WmiResults.CSDVersion
+    $OSVersionNumber = $WmiResults.Version
 
     $sql = "EXEC Windows.Host_Update $HostID, '$Domain', '$OS', '$OSArchitecture', '$OSServicePack', '$OSVersionNumber', '$HardwareModel', '$HardwareVendor', $MemorySizeGB, '$CPUType', $CoreCount"
     Invoke-Sqlcmd -Query $sql -ServerInstance "sql1" -Database "SysMetrics"
