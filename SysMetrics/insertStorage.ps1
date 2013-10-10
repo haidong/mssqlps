9fbfe50b1ef950b797506d8ea3784d2b76989f6c
haidong <haidong.ji@gmail.com>
2013-10-09 22:05:16 -0500

    fixed bug: ignore disk whose path starts with \\

--- SysMetrics/insertStorage.ps1
+++ SysMetrics/insertStorage.ps1
@@ -1,19 +1,20 @@
-$a = Invoke-Sqlcmd -Query "EXEC Windows.Host_Select_HostID_HostName" -ServerInstance "sql1" -Database "SysMetrics"
-$a | ForEach-Object {
+$HostList = Invoke-Sqlcmd -Query "EXEC Windows.Host_Select_HostID_HostName" -ServerInstance "sql1" -Database "SysMetrics"
+$HostList | ForEach-Object {
     $HostName = $_.HostName
     $HostID = $_.HostID
 #Note: DriveType 5 is CD/DVD, DriveType 2 is removable disk therefore we don't care. We only care about LocalDisk, which is DriveType 3
     try {
-            $b = get-wmiobject -computername $hostName Win32_volume | where { $_.DriveType -eq 3}
+            $WmiResults = get-wmiobject -computername $hostName Win32_volume | where { $_.DriveType -eq 3}
         }
     catch [Exception] {
         continue
     }
-    $b | foreach {
-        $DiskSizegB = ($_.Capacity / 1gb) + 1
-        $DiskFreeGB = ($_.FreeSpace / 1gb) + 1
-        $DiskPath, $DiskFormat, $DiskLabel = $_.Name, $_.FileSystem, $_.Label
-        $sql = "EXEC Windows.Storage_Insert $HostID, '$DiskPath', '$DiskFormat', '$DiskLabel', $DiskSizeGB, $DiskFreeGB"
-        Invoke-Sqlcmd -Query $sql -ServerInstance "sql1" -Database "SysMetrics"
-    }
-} 
+    $WmiResults | foreach {
+        $DiskPath = $_.Name
+        if (-not ($DiskPath.StartsWith("\\"))) {
+            $DiskSizegB = ($_.Capacity / 1gb) + 1
+            $DiskFreeGB = ($_.FreeSpace / 1gb) + 1
+            $DiskFormat, $DiskLabel = $_.FileSystem, $_.Label
+            $sql = "EXEC Windows.Storage_Insert $HostID, '$DiskPath', '$DiskFormat', '$DiskLabel', $DiskSizeGB, $DiskFreeGB"
+            Invoke-Sqlcmd -Query $sql -ServerInstance "sql1" -Database "SysMetrics"
+    }}}
