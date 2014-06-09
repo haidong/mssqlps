2363d9d62dac0e53ae7d2712abef1408c5556c04
haidong <haidong.ji@gmail.com>
2014-06-08 20:36:37 -0700

    Added tests for getSqlInstanceName function

---
+++ JiMetrics/dev/getSqlInstanceName.Tests.ps1
@@ -0,0 +1,12 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+Describe "getSqlInstanceName" {
+
+    It "sql2 has Finance and HR" {
+	$SqlInstances = getSqlInstanceName("sql2")
+	$SqlInstances.InstanceName[1] | Should Be "sql2\hr"
+	$SqlInstances.InstanceName[0] | Should Be "sql2\finance"
+    }
+}
---
+++ JiMetrics/dev/getSqlInstanceName.ps1
@@ -0,0 +1,19 @@
+﻿function getSqlInstanceName($ComputerName) {
+    $SqlInstances = Get-Service -ComputerName $ComputerName | where {($_.Name -like
+    'mssql$*') -or ($_.Name -eq 'mssqlserver')}
+    $instanceNameArray = New-Object System.Collections.ArrayList
+    if ($SqlInstances -ne $null) {
+        $SqlInstances | foreach {
+            if ($_.Name -eq 'mssqlserver') {
+                [void]
+                $instanceNameArray.add(@{InstanceName=$ComputerName;Status=$_.Status})
+            }
+            else {
+                [void]
+                $instanceNameArray.add(@{InstanceName=$ComputerName + "\" +
+                $_.Name.split("$")[1];Status=$_.Status})
+            }
+        }
+    }
+    $instanceNameArray
+}
